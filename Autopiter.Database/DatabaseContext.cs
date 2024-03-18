33a4df03f248c34dd7481abbc3a77d255f028831
bekonchik <128593810+bekonchik@users.noreply.github.com>
2024-03-18 11:26:45 +0300

    Add files via upload
    Autopiter.Database

---
+++ Autopiter.Database/DatabaseContext.cs
@@ -0,0 +1,186 @@
+﻿using Autopiter.Core.Entities;
+using Autopiter.Core.Enum;
+
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autopiter.Infrastructure
+{
+    public class DatabaseContext : DbContext
+    {
+
+        public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<CompanyBranch> CompanyBranches { get; set; }
+
+        public DbSet<Installation> Installations { get; set; }
+
+        public DbSet<PrintingDevice> PrintDevices { get; set; }
+
+        private List<CompanyBranch> _companyBranches = new List<CompanyBranch>()
+        {
+            new CompanyBranch
+            {
+                Id = 1,
+                LocationName = "Тридевятое царство",
+            },
+            new CompanyBranch
+            {
+                Id = 2,
+                LocationName = "Дремучий лес"
+            },
+            new CompanyBranch
+            {
+                Id = 3,
+                LocationName = "Луна"
+            }
+        };
+
+        private List<Employee> _employees = new List<Employee>()
+        {
+            new Employee
+            {
+                Id = 1,
+                Name = "Царь",
+                CompanyBranchId = 1
+            },
+            new Employee
+            {
+                Id = 2,
+                Name = "Добрыня",
+                CompanyBranchId = 1
+            },
+            new Employee
+            {
+                Id = 3,
+                Name = "Яга",
+                CompanyBranchId = 2
+            },
+            new Employee
+            {
+                Id = 4,
+                Name = "Копатыч",
+                CompanyBranchId = 3
+            },
+            new Employee
+            {
+                Id = 5,
+                Name = "Кощей",
+                CompanyBranchId = 3
+            },
+            new Employee
+            {
+                Id = 6,
+                Name = "Лосяш",
+                CompanyBranchId = 3
+            },
+        };
+
+        private List<PrintingDevice> _printDevices = new List<PrintingDevice>()
+        {
+            new PrintingDevice
+            {
+                Id = 1,
+                Name = "Папирус",
+                ConnectionType = ConnectionType.Local
+            },
+            new PrintingDevice
+            {
+                Id = 2,
+                Name = "Бумага",
+                ConnectionType = ConnectionType.Local
+            },
+            new PrintingDevice
+            {
+                Id = 3,
+                Name = "Камень",
+                ConnectionType = ConnectionType.Network
+            },
+        };
+
+        private List<Installation> _instalations = new List<Installation>()
+        {
+            new Installation
+            {
+                Id = 1,
+                Name = "Дворец",
+                CompanyBranchId = 1,
+                OrderNumber = 1,
+                UseIndicator = true,
+                PrintingDeviceId = 1
+            },
+            new Installation
+            {
+                Id = 2,
+                Name = "Конюшни",
+                CompanyBranchId = 1,
+                OrderNumber = 2,
+                UseIndicator = false,
+                PrintingDeviceId = 2
+            },
+            new Installation
+            {
+                Id = 3,
+                Name = "Оружейная",
+                CompanyBranchId = 1,
+                OrderNumber = 3,
+                UseIndicator = false,
+                PrintingDeviceId = 2
+            },
+            new Installation
+            {
+                Id = 4,
+                Name = "Кратер",
+                CompanyBranchId = 3,
+                OrderNumber = 1,
+                UseIndicator = true,
+                PrintingDeviceId = 3
+            },
+            new Installation
+            {
+                Id = 5,
+                Name = "Избушка",
+                CompanyBranchId = 2,
+                OrderNumber = 3,
+                UseIndicator = false,
+                PrintingDeviceId = 2
+            },
+            new Installation
+            {
+                Id = 6,
+                Name = "Топи",
+                CompanyBranchId = 2,
+                OrderNumber = 2,
+                UseIndicator = true,
+                PrintingDeviceId = 1
+            },
+        };
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> contextOptions) : base(contextOptions)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<CompanyBranch>()
+                .HasData(_companyBranches);
+
+            modelBuilder.Entity<Employee>()
+                .HasData(_employees);
+
+            modelBuilder.Entity<PrintingDevice>()
+                .HasData(_printDevices);
+
+            modelBuilder.Entity<Installation>()
+                .HasData(_instalations);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Autopiter.Database/Migrations/20240313214820_InitialCreate.cs
@@ -0,0 +1,171 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Autopiter.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CompanyBranches",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LocationName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CompanyBranches", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PrintDevices",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ConnectionType = table.Column<int>(type: "int", nullable: false),
+                    MacAdress = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PrintDevices", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CompanyBranchId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Employees_CompanyBranches_CompanyBranchId",
+                        column: x => x.CompanyBranchId,
+                        principalTable: "CompanyBranches",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Installations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CompanyBranchId = table.Column<int>(type: "int", nullable: false),
+                    PrintingDeviceId = table.Column<int>(type: "int", nullable: false),
+                    OrderNumber = table.Column<int>(type: "int", nullable: true),
+                    UseIndicator = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Installations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Installations_CompanyBranches_CompanyBranchId",
+                        column: x => x.CompanyBranchId,
+                        principalTable: "CompanyBranches",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Installations_PrintDevices_PrintingDeviceId",
+                        column: x => x.PrintingDeviceId,
+                        principalTable: "PrintDevices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "CompanyBranches",
+                columns: new[] { "Id", "LocationName", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Тридевятое царство", null },
+                    { 2, "Дремучий лес", null },
+                    { 3, "Луна", null }
+                });
+
+            migrationBuilder.InsertData(
+                table: "PrintDevices",
+                columns: new[] { "Id", "ConnectionType", "MacAdress", "Name" },
+                values: new object[,]
+                {
+                    { 1, 0, null, "Папирус" },
+                    { 2, 0, null, "Бумага" },
+                    { 3, 1, null, "Камень" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "CompanyBranchId", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "Царь" },
+                    { 2, 1, "Добрыня" },
+                    { 3, 2, "Яга" },
+                    { 4, 3, "Копатыч" },
+                    { 5, 3, "Кощей" },
+                    { 6, 3, "Лосяш" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Installations",
+                columns: new[] { "Id", "CompanyBranchId", "Name", "OrderNumber", "PrintingDeviceId", "UseIndicator" },
+                values: new object[,]
+                {
+                    { 1, 1, "Дворец", 1, 1, true },
+                    { 2, 1, "Конюшни", 2, 2, false },
+                    { 3, 1, "Оружейная", 3, 2, false },
+                    { 4, 3, "Кратер", 1, 3, true },
+                    { 5, 2, "Избушка", 3, 2, false },
+                    { 6, 2, "Топи", 2, 1, true }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_CompanyBranchId",
+                table: "Employees",
+                column: "CompanyBranchId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Installations_CompanyBranchId",
+                table: "Installations",
+                column: "CompanyBranchId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Installations_PrintingDeviceId",
+                table: "Installations",
+                column: "PrintingDeviceId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Installations");
+
+            migrationBuilder.DropTable(
+                name: "CompanyBranches");
+
+            migrationBuilder.DropTable(
+                name: "PrintDevices");
+        }
+    }
+}
